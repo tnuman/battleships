9ea6a0f1e545556dc5fb90f4931cbabface15cf4
ThomasCrul <t.c.crul@gmail.com>
2018-12-03 17:33:31 +0100

    Created messages.js and started to set up messaging

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* Go to plashscreen */
 router.get("/", function (req, res) {
     res.sendFile("splash.html", {root: "./public"});
 });
 
-/* Pressing the 'PLAY' button, returns this page */
+/* Go to gamescreen when pressing the play button */
 router.get("/play", function(req, res) {
     res.sendFile("game.html", {root: "./public"});
 });
