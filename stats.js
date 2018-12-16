4fe03bd4d562084315656b47face6a88f2f9461e
tnuman <tim.numan.2001@gmail.com>
2018-12-16 16:30:37 +0100

    Last commit, app works now on heroku

--- stats.js
+++ stats.js
@@ -1,12 +1,7 @@
-/* 
- In-memory game statistics "tracker".
- As future work, this object should be replaced by a DB backend.
-*/
-
 var gameStats = {
-    since : Date.now(),     /* since we keep it simple and in-memory, keep track of when this object was created */
+    since : Date.now(),     /* keeps track of when this object was created */
     gamesStarted : 0,       /* number of games started */
-    gamesOngoing : 0,     /* number of games currently ongoing */
+    gamesOngoing : 0,       /* number of games currently ongoing */
     shipsSunk : 0           // total number of ships that have sunk
 };
 
